8ea3779dc63b611f10dd793160c89d837486361d
Lais Bandeira <lbms@cin.ufpe.br>
2025-02-12 14:36:59 -0300

    chore(components): Ajustando onPress do botao

--- app/Moura_Auto/FindMouraAuto.tsx
+++ app/Moura_Auto/FindMouraAuto.tsx
@@ -1,60 +1,73 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import mouraColors from '@/assets/colors';
 import FindDeviceHeader from '@/components/FindDeviceHeader';
 import Button from '@/components/Button';
 import EmptyScanList from '@/components/EmptyScanList';
+import DeviceCardButton from '@/components/DeviceCardButton';
 
 const auto = require('@/assets/images/logo_auto.png');
+const moura_auto_conn_path = '/Moura_Auto/ConnectedToMouraAuto';
 
 export default function FindMouraAuto() {
+  const [loading, setLoading] = React.useState(false);
+
+  const handlePress = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-            <FindDeviceHeader loading={false} Icon={auto} type={'Moura Auto'} />
+        <FindDeviceHeader loading={loading} Icon={auto} type={'Moura Auto'} />
+      </View>
+      {loading ? (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color={mouraColors.darkYellow} />
+          <View style={styles.listDevices}>
+            <DeviceCardButton name='MLOGG-895295729' id='CA:FE:09:30' rssi={50} route={moura_auto_conn_path}></DeviceCardButton>
+            <DeviceCardButton name='MLOGG-895295729' id='CA:FE:09:30' rssi={-50} route={moura_auto_conn_path}></DeviceCardButton>
+            <DeviceCardButton name='MLOGG-895295729' id='CA:FE:09:30' rssi={50} route={moura_auto_conn_path}></DeviceCardButton>
           </View>
-      {/* <FlatList
-        data={[{ key: 'header' }]} // Adiciona um item fictício para evitar lista vazia
-        renderItem={() => null} // Mantém sem itens visíveis
-        ListHeaderComponent={
-          
-        }
-      /> */}
-      <EmptyScanList />
-      {/* <View style={styles.buttonArea}> */}
-        <Button
-          title="Buscar Dispositivos"
-          style={styles.button}
-          onPress={() => {
-            /**
-             * Só disparar scan caso não esteja carregando
-             */
-          }}
-        />
-      {/* </View> */}
+        </View>
+      ) : (
+        <EmptyScanList />
+      )}
+      <Button
+        title="Buscar Dispositivos"
+        style={styles.button}
+        onPress={handlePress}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     backgroundColor: mouraColors.white,
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'space-between',
   },
   headerContainer: {
     width: '100%',
   },
-  buttonArea: {
-    margin: 5,
-  },
   button: {
     width: '90%',
     height: '8%',
+    marginBottom: 15,
     backgroundColor: mouraColors.darkYellow,
     color: mouraColors.white,
-    marginBottom: 20,
-    margin: 5,
+  },
+  listDevices: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 20,
   },
 });
 
